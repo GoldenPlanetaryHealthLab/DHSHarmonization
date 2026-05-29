6325d1338996c1a28d2ac95adcb23115288a02f5
TinasheMTapera <tinashemtapera@gmail.com>
2026-05-29 18:37:05 -0400

    feat(flat_dump): add initial flat_dump.Rmd for data dumping
    * Create a new RMarkdown file for the dump_data function.
    * Prepare for future data processing and integration with stagecoach.
    
    feat(geospatial_agg_map): enhance geospatial analysis with tests
    
    * Load necessary libraries for geospatial analysis.
    * Add a target to read healthshed shapefile and ensure CRS match.
    * Implement spatial join to map GPS points to healthshed polygons.
    * Visualize the results and provide a summary table for future analyses.
    * Include tests to verify CRS compatibility between datasets.
    
    chore(dependencies): add DiagrammeR and update rv.lock
    
    * Add DiagrammeR as a dependency in rproject.toml.
    * Update rv.lock to include DiagrammeR and its dependencies.
    
    chore(stagecoach): add mdg_healthsheds source to manifest
    
    * Include mdg_healthsheds path in stagecoach_manifest.yml for data access.
    
    Add validation tests
    Fixes #14
    
    Geospatial Mapping for the Full Dataset
    Fixes #8

--- _targets.R
+++ _targets.R
@@ -10,7 +10,7 @@ library(DHSHarmonization)
 
 # Set target options:
 tar_option_set(
-  packages = c("DHSHarmonization") # Packages that your targets need for their tasks.
+  packages = c("DHSHarmonization"), # Packages that your targets need for their tasks.
   # format = "qs", # Optionally set the default storage format. qs is fast.
   #
   # Pipelines that take a long time to run may benefit from
@@ -43,6 +43,12 @@ tar_option_set(
   #   )
   #
   # Set other options as needed.
+  
+  # A description of the pipeline.
+  # important: use this to flag useful vs development
+  # targets in the pipeline, ie filter throwaways.
+  description = "main" 
+
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
